697ad8d6c9861c9c5f0ee3c4ee3121a03338d699
Kayo Ronald <kayoronald4321@gmail.com>
2022-01-11 17:33:27 -0300

    Update file

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const axios = require("axios");
-const mysql = require("mysql")
 const nodemailer = require("nodemailer")
 const smpt = require('./config/config')
+
+// Criando a conexão com gmail
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   host: smpt.host,
@@ -28,56 +29,15 @@ app.get('/', (req, res) => {
 
 app.post("/dialogflow", (req, res) => {
   var intername = req.body.queryResult.intent.displayName
-  // conexão com banco de dados www.remotemysql.com/phpmyadmin
-  const connection = mysql.createConnection({
-    host: process.env['HOST'],
-    user: process.env['USER'],
-    password: process.env['PASSWORD'],
-    database: process.env['DATABASE']
-  });
-  setInterval(function() {
-    connection.query('SELECT 1');
-  }, 5000);
   function deletarInscricao() {
     // pegando valores passado pelo usuario
     const email = req.body.queryResult.parameters.email;
     const option = req.body.queryResult.parameters.posso;
     if (option.toLowerCase() == 'sim' || option.toLowerCase() == "s" || option.toLowerCase() == 'yes' || option.toLowerCase() == "si") {
-      var query = 'DELETE FROM `EPICEDB` WHERE `EPICEDB`.`email` = "' + email + '"'
-      connection.query(query, (error, rows, fields) => {
-        if (error) {
-          // console.log(error)
-          return res.json({
-            fulfillmentText: 'Ocorreu um erro em nossa api :(',
-          })
-        } else if (rows.affectedRows == 0) {
-          return res.json({
-            fulfillmentMessages: [
-              {
-                payload: {
-                  richContent: [
-                    [
-                      {
-                        "rawUrl": "https://i.ibb.co/qDM90Vs/30723-data-animation.gif",
-                        "type": "image"
-                      },
-                      {
-                        "type": "button",
-                        "text": `Este e-mail: ${email} não está cadastrado em nosso banco de dados.`,
-                        "icon": {
-                          "color": "#fd6584",
-                          "type": "info"
-                        }
-                      }
-                    ]
-                  ]
-                }
-              }
-            ]
-          })
-        } else {
-          connection.end();
-          console.log(rows.affectedRows)
+      const data = { email }
+      async function deletarEmail(data) {
+        try {
+          await axios.post('https://epice-app.vercel.app/api/deleteUser', data)
           return res.json({
             fulfillmentMessages: [
               {
@@ -102,8 +62,18 @@ app.post("/dialogflow", (req, res) => {
               }
             ]
           })
+        } catch (error) {
+          console.log(error.status)
+          if(error.message === 'Request failed with status code 401'){
+            return res.json({
+            fulfillmentText: 'Este email não está cadastrado em nosso banco de dados',
+          })
+          }
+          return res.json({
+            fulfillmentText: 'Ocorreu um erro em nossa api :(',
+          })
         }
-      })
+      } deletarEmail(data)
     } else if (option.toLowerCase() == 'não' || option.toLowerCase() == "n" || option.toLowerCase() == 'nao' || option.toLowerCase() == "no") {
       return res.json({
         fulfillmentText: 'Não deleteamos nada'
@@ -115,64 +85,14 @@ app.post("/dialogflow", (req, res) => {
     const name = req.body.queryResult.parameters.name;
     const email = req.body.queryResult.parameters.email;
     const curso = req.body.queryResult.parameters.curso;
-    var query = 'insert into EPICEDB (name, email, curso) values ("' + name + '","' + email + '", "' + curso + '")'
-    connection.query(query, (error, results, fields) => {
-      if (error) {
-        console.log(error)
-        if (error.code === 'ER_DUP_ENTRY') {
-          return res.json({
-            fulfillmentText: `${name.split(' ')[0]}, esté email já foi cadastro`,
-          })
-        }
-        return res.json({
-          fulfillmentText: 'Ocorreu um erro em nossa api :(',
-        })
-      }
-      connection.end();
-      return res.json({
-        fulfillmentMessages: [
-          {
-            payload: {
-              richContent: [
-                [
-                  {
-                    "rawUrl": "https://digitalsynopsis.com/wp-content/uploads/2015/10/gif-icons-menu-transition-animations-shake-new-mail.gif",
-                    "type": "image"
-                  },
-                  {
-                    "type": "button",
-                    "text": `Seja bem vindo(a): ${name.split(' ')[0]}, sua conta foi criada com sucesso`,
-                    "icon": {
-                      "color": "#FF9800",
-                      "type": "check_circle"
-                    }
-                  }
-                ]
-              ]
-            }
-          }
-        ]
-      })
-    })
-  }
-  function sendEmail() {
-    const nome = req.body.queryResult.parameters.name;
-    const email = req.body.queryResult.parameters.email;
-    const assunto = req.body.queryResult.parameters.assunto;
-    const mensagem = req.body.queryResult.parameters.mensagem;
-    console.log(nome)
-    async function run() {
+    const data = {
+      name,
+      email,
+      curso
+    }
+    async function createUser(data) {
       try {
-        await transporter.sendMail({
-          from: email,
-          to: smpt.user,
-          subject: `${nome.split(' ')[0]} - ${assunto}`,
-          html: `
-          <b>Nova mensagem de ${nome}</b>
-          </br>
-          <p>${mensagem}</p>
-          `, // html body
-        })
+        await axios.post('https://epice-app.vercel.app/api/subscription', data)
         return res.json({
           fulfillmentMessages: [
             {
@@ -180,12 +100,12 @@ app.post("/dialogflow", (req, res) => {
                 richContent: [
                   [
                     {
-                      "rawUrl": "https://digitalsynopsis.com/wp-content/uploads/2015/10/gif-icons-menu-transition-animations-shake-new-mail.gif",
+                      "rawUrl": "https://i.ibb.co/WFdcS20/75705-welcome-animation.gif",
                       "type": "image"
                     },
                     {
                       "type": "button",
-                      "text": `Olá ${nome.split(' ')[0]}, foi enviada com sucesso!`,
+                      "text": `Seja bem vindo(a): ${name.split(' ')[0]}, sua conta foi criada com sucesso`,
                       "icon": {
                         "color": "#FF9800",
                         "type": "check_circle"
@@ -197,15 +117,70 @@ app.post("/dialogflow", (req, res) => {
             }
           ]
         })
-      } catch (err) {
+      } catch (error) {
+        console.log(error)
+        if (error.code === 'ER_DUP_ENTRY') {
+          return res.json({
+            fulfillmentText: `${name.split(' ')[0]}, esté email já foi cadastro`,
+          })
+        }
+        return res.json({
+          fulfillmentText: 'Ocorreu um erro em nossa api :(',
+        })
+      }
+    } createUser(data)
+  }
+  function sendEmail() {
+    const nome = req.body.queryResult.parameters.name;
+    const email = req.body.queryResult.parameters.email;
+    const assunto = req.body.queryResult.parameters.assunto;
+    const mensagem = req.body.queryResult.parameters.mensagem;
+    // Realizando o envio de email
+    (async function() {
+      try {
+        await transporter.sendMail({
+          from: email,
+          to: smpt.user,
+          replyTo: email,
+          subject: `${nome.split(' ')[0]} - ${assunto}`,
+          html: `
+          <b>Nova mensagem de ${nome}</b>
+          </br>
+          <p>${mensagem}</p>
+          `, // criando a mensagem
+        })
+        return res.json({
+          fulfillmentMessages: [
+            {
+              payload: {
+                richContent: [
+                  [
+                    {
+                      "rawUrl": "https://i.ibb.co/zbGN1Mr/71167-email-ent.gif",
+                      "type": "image"
+                    },
+                    {
+                      "type": "button",
+                      "text": `Olá ${nome.split(' ')[0]}, seu email foi enviado com sucesso!`,
+                      "icon": {
+                        "color": "#58cb42",
+                        "type": "check_circle"
+                      }
+                    }
+                  ]
+                ]
+              }
+            }
+          ]
+        })
+      } catch (err) {//caso não seja enviado, vai cai dentro do catch
         console.log(err)
         return res.json({
           fulfillmentText: 'Ops.. meu serviço está em manutenção :('
         })
       }
-    } run()
+    })();
   }
-  console.log(intername)
   switch (intername) {
     case 'subscription':
       createUSerMysql()
@@ -219,6 +194,6 @@ app.post("/dialogflow", (req, res) => {
   }
 });
 
-const listener = app.listen(3000, function() {
-  console.log("rondando");
+app.listen(3000, function() {
+  console.log("FUego");
 });
